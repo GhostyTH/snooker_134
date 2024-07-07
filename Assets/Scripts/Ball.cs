8b1bd44f78a07c1c2cb8ba4cfd267c9fc862601d
GhostyTH <Tang9841@gmail.com>
2024-07-07 01:53:42 -0700

    project_ready_update_2

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,8 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public enum BallColor
 {
+    White,
+    Red,
+    Yellow,
+    Green,
+    Brown,
+    Blue,
+    Pink,
+    Black
+}
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private int point;
+
+    [SerializeField]
+    private BallColor color;
+
+
     // Start is called before the first frame update
     void Start()
     {
