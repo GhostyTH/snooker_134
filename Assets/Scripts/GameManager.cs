441c0b40aa3ff1ee4e42932c5f38fee1ee7402ee
GhostyTH <Tang9841@gmail.com>
2024-07-14 10:17:44 +0700

    project_update_5

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
