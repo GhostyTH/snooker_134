c1277db26ed7b2d55fe7d84420eb6220e621d478
GhostyTH <Tang9841@gmail.com>
2024-08-04 14:19:01 +0700

    commit_setballcolor

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -18,20 +18,64 @@ public class Ball : MonoBehaviour
 {
     [SerializeField]
     private int point;
+    public int Point { get { return point; } set { point = value; } }
 
     [SerializeField]
     private BallColor color;
 
+    [SerializeField]
+    private MeshRenderer rd;
+
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rd = GetComponent<MeshRenderer>();
     }
+    
+    public void SetColorAndPoint(BallColor col)
+    {
+        switch(col)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = new Color32(245, 120, 0, 255);
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = new Color32(255, 105, 224, 255);
+                break;
+            case BallColor.Black: 
+                point = 7;
+                rd.material.color = Color.black;
+                break;
+        }
+    }
+}
 
-    private void OnMouseDown()
+/*    private void OnMouseDown()
     {
         Debug.Log(point);
         Destroy(gameObject);
     }
-}
+}*/
