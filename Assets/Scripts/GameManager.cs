e26a5eff36da5e2eb57025ad1261ec9f2bfffa8b
GhostyTH <Tang9841@gmail.com>
2024-08-04 15:45:11 +0700

    commit_shootandrotate

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,16 +13,56 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject[] ballPositions;
 
+    [SerializeField]
+    private GameObject ballPrefab;
+
+    [SerializeField]
+    private GameObject cueBall;
+
+    [SerializeField]
+    private float xInput;
+
     public static GameManager instance;
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        SetBall(BallColor.Red, 1);
+        SetBall(BallColor.Yellow, 2);
+        SetBall(BallColor.Green, 3);
+        SetBall(BallColor.Brown, 4);
+        SetBall(BallColor.Blue, 5);
+        SetBall(BallColor.Pink, 6);
+        SetBall(BallColor.Black, 7);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            ShootBall();
+            RotateBall();
+    }
+
+    private void ShootBall()
+    {
+        Rigidbody rd = cueBall.GetComponent<Rigidbody>();
+        rd.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+    }
+
+    private void SetBall(BallColor col, int i)
+    {
+        GameObject obj = Instantiate(ballPrefab,
+                                    ballPositions[i].transform.position,
+                                    Quaternion.identity);
+        Ball b = obj.GetComponent<Ball>();
+        b.SetColorAndPoint(col);
+    }
+
+    private void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cueBall.transform.Rotate(new Vector3(0f, xInput, 0f));
     }
 }
